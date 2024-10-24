0b92994ee50ae348040acd25921c08e1490c2958
FExeZ <francoexequielzerda@gmail.com>
2024-10-24 19:40:51 -0300

    First Commit

---
+++ src/test/java/ar/com/fez/tech/APIRest/ApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.fez.tech.APIRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
